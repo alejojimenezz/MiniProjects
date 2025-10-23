d8a6b6bdb5568331a98e2ba9d31e44be9a725c35
alejojimenezz <ajimenezz@unal.edu.co>
2025-10-22 22:19:36 -0500

    chore: :construction: Begin cuadratic formula file challenge

--- CodeExpress/Basic/EcuacionSegundoGrado.py
+++ CodeExpress/Basic/EcuacionSegundoGrado.py
@@ -12,3 +12,22 @@
 #Cuando termines, le haces Commit y sincronizas los cambios a tu repositorio con GitHub. Ya nosotros nos encargaremos del resto.
 
 #Hablaste con el Vice de Computer Society UN. Suerte en el reto :)
+
+from decimal import *
+import math
+
+a = int(input("Ingrese el valor de a: "))
+b = int(input("Ingrese el valor de b: "))
+c = int(input("Ingrese el valor de c: "))
+
+# ax^2+bx+c=0
+
+delta = b**2-4*a*c
+
+if delta < 0:
+    print("Sin raíces reales")
+elif a == 0:
+    print(delta)
+elif a != 0:
+    x1 = (-b+math.sqrt(delta))/2*a
+    x2 = (-b-math.sqrt(delta))/2*a
