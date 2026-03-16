1c914892c6c1f4ef6e3ee2a9cd7c53fa9453ba6f
Alejandro Jimenez <alejandro.jz1312@gmail.com>
2026-03-16 13:50:22 -0500

    style: :art: Base format for QR generation

--- QRGenerator/generator.py
+++ QRGenerator/generator.py
@@ -1,9 +1,12 @@
 # Requires qrcode library
 # Run 'pip install "qrcode[pil]"'
+# Run 'pip install "qrcode[svg]"'
 
 import qrcode
+from PIL import Image
 
-data = "https://www.instagram.com/"
+data = "https://www.google.com/"
+
 img = qrcode.make(data)
 
 img.save("QRGenerator/codeGenerated.png")
