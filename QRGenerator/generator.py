5ec5390d11e1752fe721f2699c4481f5340a9b50
Alejandro Jimenez <alejandro.jz1312@gmail.com>
2026-04-22 17:43:52 -0500

    chore: :building_construction: UV adaptation

--- QRGenerator/generator.py
+++ QRGenerator/generator.py
@@ -1,3 +1,10 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "qrcode[pil]>=8.2",
+# ]
+# ///
+
 # Requires qrcode library
 # Run 'pip install "qrcode[pil]"'
 # Run 'pip install "qrcode[svg]"'
@@ -5,9 +12,9 @@
 import qrcode
 from PIL import Image
 
-data = "https://www.google.com/"
+data = "https://docs.google.com/document/d/1qvt4YvwJa0hMZR4OmAT7LK3prnYxmUFf/edit?usp=sharing&ouid=101215009529947416218&rtpof=true&sd=true"
 
 img = qrcode.make(data)
 
-img.save("QRGenerator/codeGenerated.png")
+img.save("C:/Users/ThinkPad/Documents/VSCode/MiniProjects/QRGenerator/codeGenerated.png")
 print("QR code generated and saved as codeGenerated.png")
